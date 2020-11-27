b40e32038cbcb8c3a896f81be5bc18829f4ba570
baguevaram <baguevaram@unal.edu.co>
2020-11-27 10:55:18 -0500

    :sparkles: text and explosion

--- game.py
+++ game.py
@@ -1,12 +1,17 @@
 import sys, pygame
+from math import pi, sin, cos
 
 # Inicializamos pygame
 pygame.init()
 # Muestro una ventana de 800x600
-size = 800, 600
+size = 1000, 600
 screen = pygame.display.set_mode(size)
+
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
 # Inicializamos variables
-width, height = 800, 600
+width, height = 1000, 600
 speed = [1, 1]
 
 background = pygame.image.load("images/background.png")
@@ -26,10 +31,29 @@ fireBall = pygame.image.load("images/fireBall.png")
 fireBall = pygame.transform.scale(fireBall, (20, 20))
 fireBallRect = fireBall.get_rect()
 
+explosion = pygame.image.load("images/explosion.png")
+explosion = pygame.transform.scale(explosion, (50, 50))
+explosionRect = explosion.get_rect()
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+textWin = font.render('Tank 1 Wins', True, green, blue)
+
+# create a rectangular object for the
+# text surface object
+textWinRect = textWin.get_rect()
+
+# set the center of the rectangular object.
+textWinRect.center = (width // 2, height // 2)
 
 fire = False
+win = False
 
 run = True
+
+gravityAcceleration = 0.8  # positivo porque el el juego el positivo es hacia abajo
+vel = 20
+
 while run:
     pygame.time.delay(0)
 
@@ -37,31 +61,47 @@ while run:
         # Si el evento es salir de la ventana, terminamos
         if event.type == pygame.QUIT: run = False
 
+    screen.blit(background, [0, 0])
+
+    screen.blit(tank1, tank1rect)
+    screen.blit(tank2, tank2rect)
+
+    if win:
+        screen.blit(textWin, textWinRect)
+        screen.blit(explosion, explosionRect)
+        pygame.display.flip()
+        continue
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d]:
         tank1rect = tank1rect.move(1, 0)
     if keys[pygame.K_a]:
         tank1rect = tank1rect.move(-1, 0)
-    if keys[pygame.K_j] and not fire:
 
+    if keys[pygame.K_j] and not fire:
+        angle = 45 * pi / 180
+        vx = vel * cos(angle)
+        vy = -vel * sin(angle)  # coordenadas en y en el juego van en sentido contrario
         fire = True
-        fireBallRect.x=150
-        fireBallRect.y=150
+        fireBallRect.x = tank1rect.x
+        fireBallRect.y = tank1rect.y
 
     if fire:
         screen.blit(fireBall, fireBallRect)
-        fireBallRect = fireBallRect.move(1, 1.8)
+        fireBallRect = fireBallRect.move(vx, vy)
+        vy += gravityAcceleration * 1
+        if fireBallRect.colliderect(tank2rect):
+            explosionRect.x = tank2rect.x
+            explosionRect.y = tank2rect.y
+            screen.blit(explosion, explosionRect)
+
+            print("tank1 WINS")
+            win = True
+            fire = False
 
     if fireBallRect.bottom > height:
         fire = False
 
-
-
-    screen.blit(background, [0, 0])
-    # screen.blit(fireBall, fireBallRect)
-
-    screen.blit(tank1, tank1rect)
-    screen.blit(tank2, tank2rect)
     pygame.display.flip()
-    # Salgo de pygame
+
 pygame.quit()
